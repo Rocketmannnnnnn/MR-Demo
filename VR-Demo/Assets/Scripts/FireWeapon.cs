78937616f46075882f5a227579e9de316ac9e018
Jasper <j.bonefaas@student.avans.nl>
2021-02-26 11:06:44 +0100

    Added M4 rifle

---
+++ VR-Demo/Assets/Scripts/FireWeapon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(OVRGrabbable))]
+public class FireWeapon : MonoBehaviour
+{
+    private OVRGrabbable grabbable;
+    private float lastFire = 0;
+    public float fireDelay = 0.5f;
+    public float bulletLifeTime = 5.0f;
+    public float shellSpeed = 0.0f;
+    public GameObject bullet;
+    public Transform barrelEnd;
+
+    private void Awake()
+    {
+        this.grabbable = GetComponent<OVRGrabbable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.grabbable.isGrabbed)
+        {
+            if(this.grabbable.grabbedBy.m_controller == OVRInput.Controller.LTouch)
+            {
+                CheckInput(OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger));
+            }
+            else
+            {
+                CheckInput(OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger));
+            }
+        }
+    }
+
+    private void CheckInput(bool isPressed)
+    {
+        if(isPressed && Time.time > lastFire + fireDelay)
+        {
+            lastFire = Time.time;
+            GameObject shellToShoot = Instantiate(bullet, barrelEnd.position, barrelEnd.rotation);
+            shellToShoot.GetComponent<Rigidbody>().velocity = shellToShoot.transform.forward * shellSpeed;
+            Destroy(shellToShoot, bulletLifeTime);
+        }
+    }
+}
---
+++ VR-Demo/Assets/Scripts/ShellController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellController : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.root.gameObject.tag == "Enemy")
+            Destroy(collision.transform.root.gameObject);
+        Destroy(this.transform.root.gameObject);
+    }
+}
